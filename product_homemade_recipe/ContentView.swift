110c6c1b807c41199cba649ec5fdb5fcadd6d9a5
Omar MHAIMDAT <omarmhaimdat@gmail.com>
2020-08-29 10:30:03 +0100

    Initial Commit

---
+++ product_homemade_recipe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  product_homemade_recipe
+//
+//  Created by M'haimdat omar on 29-08-2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ product_homemade_recipe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  product_homemade_recipe
+//
+//  Created by M'haimdat omar on 29-08-2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
